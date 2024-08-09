d6fb5b32279965b8a41ddf180b403347b212bd4c
Devika <devikabmohan4@gmail.com>
2024-08-09 15:23:19 +0530

    swap numbers

---
+++ swap numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b;
+    cout<<"a="<<a<<endl<<"b="<<b<<endl<<"After swapping";
+    c=a;
+    a=b;
+    b=c;
+    cout<<endl<<"a="<<a<<endl<<"b="<<b;
+    return 0;
+}
