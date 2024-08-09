ef59b95b4407d266a3df4e7524a8aea471d7864f
Devika <devikabmohan4@gmail.com>
2024-08-09 15:36:20 +0530

    trial

---
+++ git trial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int b;
+    string a;
+    cout<<"Enter name ";
+    cin>>a;
+    cout<<endl<<a;
+    cout<<"Enter age:";
+    cin>>b;
+    cout<<endl<<b;
+    return 0;
+}
